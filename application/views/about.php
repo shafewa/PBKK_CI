3c21a130faf9ea8b45527977e729850a0eb6f4b8
shafewa <47876619+shafewa@users.noreply.github.com>
2020-02-15 13:31:16 +0700

    Add files via upload

---
+++ application/views/about.php
@@ -0,0 +1,2 @@
+<h1>About Web</h1>
+<p>Web ini dibuat pada 15/februari/2020</p>
---
+++ application/views/contact.php
@@ -0,0 +1,20 @@
+<h1>Contact</h1>
+
+<table>
+    <tr>
+        <td><b>Nama :</b></td>
+        <td>Muh. Shafwatallah Wahid</td>
+    </tr>
+    <tr>
+        <td><b>HP/WA :</b></td>
+        <td>082290454XXX</td>
+    </tr>
+    <tr>
+        <td><b>Email :</b></td>
+        <td>Shafwatallah@gmail.com</td>
+    </tr>
+    <tr>
+        <td><b>Blog</b></td>
+        <td><a href="http://cokinabalao.blogspot.com/" target="_blank">cokinabalao.blogspot.com</a></td>
+    </tr>
+</table>
